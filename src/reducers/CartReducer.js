996ae1d5b16c220b48d360cdc6bc5b5ea63f7a2e
RTEYL <tyler-w-jones@hotmail.com>
2020-11-05 15:15:47 -0800

    add cart action/reducer

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -8,10 +8,11 @@ export default function cartReducer(state = initialState, { type, payload }){
   switch (type) {
 
   case 'ADD_TO_CART':
-    return { ...state, items: state.items.concat(payload.item) }
+    debugger
+    return { ...state, items: state.items.concat(payload), itemCount: state.itemCount + 1 }
 
   case 'REMOVE_FROM_CART':
-    return {...state, items: state.items.filter(item=>item.id !== payload.item.id)}
+    return {...state, items: state.items.filter(item=>item.id !== payload.item.id), itemCount: state.itemCount - 1}
   default:
     return state
   }
