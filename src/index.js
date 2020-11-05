139e1b0a0a1c543bc8054ee91c5c409b878f9bd1
RTEYL <tyler-w-jones@hotmail.com>
2020-11-04 18:09:04 -0800

    add thunk middleware

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux'
-import {createStore,combineReducers} from 'redux'
+import {createStore,combineReducers, applyMiddleware} from 'redux'
 import CartReducer from './reducers/CartReducer'
 import ItemsReducer from './reducers/ItemsReducer';
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
   cart: CartReducer,
   items: ItemsReducer
 })
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
