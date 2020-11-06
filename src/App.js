8b7fc5a9b3239f9bb2e777dfe558b29b6fed3856
RTEYL <tyler-w-jones@hotmail.com>
2020-11-05 17:26:56 -0800

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react'
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ItemsContainer from './containers/ItemsContainer';
 import NavBar from './components/NavBar'
 import {fetchItems} from './actions/Fetch'
 import {connect} from 'react-redux'
+import CartContainer from './containers/CartContainer'
 
 class App extends Component{
   render(){
     return(
       <>
         {this.props.fetchItems()}
-        <NavBar />
-        <ItemsContainer />
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path='/' component={ItemsContainer}/>
+            {/* <Route exact path='/about' component={About}/> */}
+            {/* <Route component={NoMatch}/> */}
+          </Switch>
+          <CartContainer />
+        </Router>
       </>
       )
   }
