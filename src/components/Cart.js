85d6c925ee9821b5345ce00ac25271c6e1714f57
RTEYL <tyler-w-jones@hotmail.com>
2020-11-06 20:00:57 -0800

    add cuid to cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+import cuid from 'cuid'
 
 export default class Cart extends Component {
   render() {
     return (
       <div className='cart'>
         {this.props.items.map((item) => (
-          <li key={item.id}>
+          <li key={cuid()}>
+              <p>{ item.count + 'X' } </p>
               <p>{item.name} <button onClick={()=>this.props.removeFromCart(item)} >x</button></p>
-              <p>{item.price}</p>
+              <p>${item.price * item.count}</p>
           </li>
             )
           )
