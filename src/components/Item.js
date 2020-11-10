c31166084aaa875e3159cbf6df25d6c2f239e9c5
RTEYL <tyler-w-jones@hotmail.com>
2020-11-09 18:42:53 -0800

    use react-magnify-image

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import { Card, Col } from 'react-bootstrap'
+import ReactImageMagnify from 'react-image-magnify';
 
 export default class Item extends Component {
   render() {
     return (
-      <Col sm={5} md={3} lg={true} >
+      <Col sm={6.5} md={6} lg={4} xl={3} >
         <Card bg='light'>
-          <Card.Img src={this.props.item.image} alt={this.props.item.name} />
+          <ReactImageMagnify {...{
+                        smallImage: {
+                            alt: this.props.item.name,
+                            isFluidWidth: true,
+                            src: this.props.item.image,
+                            sizes: '(max-width: 480px) 100vw, (max-width: 1200px) 30vw, 360px'
+                        },
+                        largeImage: {
+                            src: this.props.item.image,
+                            width: 800,
+                            height: 1000,
+                        },
+                        enlargedImagePosition: 'over',
+                        isHintEnabled: true,
+                        shouldHideHintAfterFirstActivation: false
+
+                    }} />
           <Card.Body>
             <Card.Title>{this.props.item.name}</Card.Title>
             <Card.Text>
