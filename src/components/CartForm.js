9e4e72d5041f235ac01933d5f508e1041bcb036f
RTEYL <tyler-w-jones@hotmail.com>
2020-11-10 19:42:58 -0800

    create form layout

---
+++ src/components/CartForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class CartForm extends Component {
+  state = {
+    email: '',
+    address: '',
+    city: '',
+    state: '',
+    zipCode: '',
+    phoneNumber: '',
+    ccNum: '',
+    ccCVC: '',
+    shippingMethod: '',
+    totalCost: 0,
+    isGift: false
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
