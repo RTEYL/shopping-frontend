281184cf4f251abaad1ae026fe954d61075edcbb
RTEYL <tyler-w-jones@hotmail.com>
2020-11-12 15:32:52 -0800

    fetch to see if a user is logged in

--- src/actions/Fetch.js
+++ src/actions/Fetch.js
@@ -3,5 +3,15 @@ export function fetchItems(){
     fetch('http://localhost:3000/api/v1/items')
     .then(resp=>resp.json())
     .then(json=>dispatch({type: 'ADD_ITEM', payload: json}))
+    .catch(err=>console.log('fetch error', err))
+  }
+}
+
+export function fetchLoggedInUser(){
+  return async (dispatch) => {
+    fetch('http://localhost:3000/logged_in',{withCredentials: true})
+    .then(resp=>resp.json())
+    .then(json=>dispatch({type: 'IS_LOGGED_IN', payload: json}))
+    .catch(err=>console.log('fetch error', err))
   }
 }
