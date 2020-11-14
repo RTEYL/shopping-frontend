575f9f7b7975e69954266753af999aee9a1f351e
RTEYL <tyler-w-jones@hotmail.com>
2020-11-13 18:43:32 -0800

    add login/logout actions

--- src/actions/Fetch.js
+++ src/actions/Fetch.js
@@ -15,3 +15,43 @@ export function fetchLoggedInUser(){
     .catch(err=>console.log('fetch error', err))
   }
 }
+
+export function loginUser(user){
+  let configObj = {
+    withCredentials: true,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(user)
+  };
+  return (dispatch) => {
+    fetch('http://localhost:3000/login',configObj)
+    .then(resp=>resp.json())
+    .then(json=>{
+      if (!!json.errors) {
+         throw new Error().message = json.errors
+      }else{
+        dispatch({type: 'LOG_IN', payload: json})
+      }
+    })
+    .catch(err =>{
+      dispatch({type: 'LOG_IN_ERROR', payload: err})
+      })
+  }
+}
+
+export function logoutUser(){
+  return (dispatch) => {
+    fetch('http://localhost:3000/logout', {
+      withCredentials: true,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    .then(resp=>{console.log(resp.json())})
+  }
+}
