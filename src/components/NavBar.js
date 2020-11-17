37947aa836b4f3420e88bf38b5755a9d7b424936
RTEYL <tyler-w-jones@hotmail.com>
2020-11-16 21:02:16 -0800

    remove whitespace

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,25 @@
 import {Navbar, Nav} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-const NavBar = (props) => {
-    return (
-      <Navbar collapseOnSelect bg='light' expand='lg' sticky='top' >
-        <Navbar.Brand as={Link} to="/">Shopping Center</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link eventKey='1' as={Link} to="/">Home</Nav.Link>
-            <Nav.Link eventKey='2' as={Link} to="/about">About</Nav.Link>
-            {props.loggedIn ?
-              <Nav.Link eventKey='3' as={Link} to="/logout">Logout</Nav.Link>
-               :
-             <Nav.Link eventKey='4' as={Link} to="/login">Login</Nav.Link>
-            }
-            <Nav.Link eventKey='5' as={Link} to="/cart">Cart</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
+const NavBar = ({loggedIn}) => {
+  console.log(loggedIn);
+  return (
+    <Navbar collapseOnSelect bg='light' expand='lg' sticky='top' >
+      <Navbar.Brand as={Link} to="/">Shopping Center</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link eventKey='1' as={Link} to="/">Home</Nav.Link>
+          <Nav.Link eventKey='2' as={Link} to="/about">About</Nav.Link>
+          {loggedIn ?
+            <Nav.Link eventKey='3' as={Link} to="/logout">Logout</Nav.Link>
+              :
+            <Nav.Link eventKey='4' as={Link} to="/login">Login</Nav.Link>
+          }
+          <Nav.Link eventKey='5' as={Link} to="/cart">Cart</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 export default NavBar
