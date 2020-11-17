f85c5e020eb5359da2ffecc9b84844e437f76af5
RTEYL <tyler-w-jones@hotmail.com>
2020-11-16 21:02:09 -0800

    fix bug on login check

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,19 +1,20 @@
 const initialState = {
-  user: {},
-  logged_in: false
+  user: {}
 }
 
 export default function UserReducer(state = initialState, { type, payload }){
   switch (type) {
 
   case 'IS_LOGGED_IN':
-    return { ...state, logged_in: payload.logged_in }
+    return { ...state, user: payload.user,  logged_in: payload.logged_in }
   case 'LOG_IN':
     return { ...state, user: payload.user, logged_in: payload.logged_in }
   case 'LOG_OUT':
-    return { ...state, logged_in: payload.logged_in }
+    return { ...state, user: {}, logged_in: payload.logged_in }
   case 'AUTH_ERROR':
-    return { ...state, user: { errors: payload}}
+    return { ...state, user: { ...state.user, errors: payload}}
+  case 'REMOVE_ERRORS':
+    return {...state, user: { ...state.user, errors: '' }}
 
   default:
     return state
