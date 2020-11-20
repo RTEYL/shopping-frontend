037abb2d6d3faf735ac18a285ebbdd8c1806070b
RTEYL <tyler-w-jones@hotmail.com>
2020-11-19 18:27:59 -0800

    add checkout to reducer

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -9,28 +9,31 @@ let i;
 export default function cartReducer(state = initialState, { type, payload }){
   switch (type) {
 
-  case 'ADD_TO_CART':
-    if (!payload.count || payload.count === 0) {
-      payload.count = 1
-      return { ...state, items: state.items.concat(payload), itemCount: state.itemCount + 1, active: true }
-    }else{
-      i = state.items.indexOf(payload)
-      state.items[i].count += 1
-      return { ...state, items: [...state.items], itemCount: state.itemCount + 1, active: true }
-    }
+    case 'ADD_TO_CART':
+      if (!payload.count || payload.count === 0) {
+        payload.count = 1
+        return { ...state, items: state.items.concat(payload), itemCount: state.itemCount + 1, active: true }
+      }else{
+        i = state.items.indexOf(payload)
+        state.items[i].count += 1
+        return { ...state, items: [...state.items], itemCount: state.itemCount + 1, active: true }
+      }
 
-  case 'REMOVE_FROM_CART':
-    if (payload.count === 1) {
-      payload.count -=1
-      return {...state, items: state.items.filter(item=>item.id !== payload.id), itemCount: state.itemCount - 1}
-    }else{
-      i = state.items.indexOf(payload)
-      state.items[i].count -= 1
-      return {...state, items: [...state.items], itemCount: state.itemCount - 1}
-    }
+    case 'REMOVE_FROM_CART':
+      if (payload.count === 1) {
+        payload.count -=1
+        return {...state, items: state.items.filter(item=>item.id !== payload.id), itemCount: state.itemCount - 1}
+      }else{
+        i = state.items.indexOf(payload)
+        state.items[i].count -= 1
+        return {...state, items: [...state.items], itemCount: state.itemCount - 1}
+      }
 
     case 'SET_ACTIVE':
       return {...state, active: payload}
+
+    case 'CHECK_OUT':
+      return {...state, initialState}
   default:
     return state
   }
