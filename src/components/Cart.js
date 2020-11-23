68988d2a20a94142a94bbd8de945f7be82fca209
RTEYL <tyler-w-jones@hotmail.com>
2020-11-22 17:59:26 -0800

    add small thumbnail to cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,22 @@
 import cuid from 'cuid'
-import { Button, ListGroup } from 'react-bootstrap'
+import { Button, Image, Media } from 'react-bootstrap'
 
 const Cart = (props) => {
   return (
     <div className={'cart'}>
       {props.items.map((item) => (
-        <ListGroup variant="flush" key={cuid()}>
-          <ListGroup.Item>
+        <Media as='li' key={cuid()}>
+            <Image width={100} height={100} src={item.image} alt={item.name} fluid rounded/>
+          <Media.Body>
             {item.name}
              <Button size="sm" variant='danger' onClick={()=>props.removeFromCart(item)} >x</Button>
             <br/>
             <p>${item.price * item.count + ' - '}
             <small>{ item.count } x ${ item.price }</small>
             </p>
-          </ListGroup.Item>
-        </ListGroup>
+          <br/><br/>
+          </Media.Body>
+        </Media>
           )
         )
       }
