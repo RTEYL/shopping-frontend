33ed7459100ba92416b6a4110098946c576731a1
RTEYL <tyler-w-jones@hotmail.com>
2020-11-22 18:00:23 -0800

    add dropdown buttons to sort by

--- src/containers/ItemsContainer.js
+++ src/containers/ItemsContainer.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Items from '../components/Items'
 import { addToCart } from '../actions/CartActions'
-import  {Container, Row}  from 'react-bootstrap'
+import  {Container, Dropdown, DropdownButton, Row}  from 'react-bootstrap'
+import { sortByPrice } from '../actions/ItemActions'
 
 class ItemsContainer extends Component {
 
+  handleClick = (event) => {
+    this.props.sortByPrice(this.props.items, event.target.value)
+  }
 
   render() {
     return (
-        <Container className='items-container'>
-          <Row>
-            <Items addToCart={this.props.addToCart} items={this.props.items}/>
-          </Row>
-        </Container>
+      <Container className='items-container'>
+        <DropdownButton id="dropdown-item-button" title="Sort By: ">
+          <Dropdown.Item onClick={this.handleClick} as="button" value='lowest'>price - lowest</Dropdown.Item>
+          <Dropdown.Item onClick={this.handleClick} as="button" value='highest'>price - highest</Dropdown.Item>
+        </DropdownButton>
+        <Row>
+          <Items addToCart={this.props.addToCart} items={this.props.items}/>
+        </Row>
+      </Container>
     )
   }
 }
@@ -26,7 +34,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps =( dispatch) => {
   return {
-    addToCart: item => dispatch(addToCart(item))
+    addToCart: item => dispatch(addToCart(item)),
+    sortByPrice: (items, method) => dispatch(sortByPrice(items, method))
   }
 }
 
