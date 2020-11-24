1276d89b333211cd10d458145386fd4151ed7af3
RTEYL <tyler-w-jones@hotmail.com>
2020-11-23 18:32:09 -0800

    create order display

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Card, Col } from 'react-bootstrap'
 
 const Order = (props) => {
   return (
-    <div className='order'>
-      order
-      {props.order.id}
-    </div>
+    <Col sm={6.5} md={6} lg={4} xl={3} >
+      <Card bg='light'>
+        <Card.Body>
+          <Card.Title>Order number: {props.order.id}</Card.Title>
+          <Card.Text>
+            <p>Purchase date: {props.order.purchase_date}</p>
+            <p>Shipped to: </p>
+            <p>Address: {props.order.line1}, {props.order.city}, {props.order.state}, {props.order.postal_code} </p>
+            <p>Email: <em>{props.order.email}</em></p>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 }
 
