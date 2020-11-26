905655aaa5748f54722ca0bf0f17c0eec016c372
RTEYL <tyler-w-jones@hotmail.com>
2020-11-25 18:38:59 -0800

    add: remove item

--- src/reducers/ItemsReducer.js
+++ src/reducers/ItemsReducer.js
@@ -1,16 +1,21 @@
 const initialState = {
   items: [],
-}
+};
 
 export default function itemsReducer(state = initialState, { type, payload }) {
   switch (type) {
+    case "ADD_ITEM":
+      return { ...state, items: state.items.concat(payload) };
 
-  case 'ADD_ITEM':
-    return { ...state, items: state.items.concat(payload)}
+    case "SORT_ITEMS":
+      return { ...state, items: payload };
 
-  case 'SORT_ITEMS':
-    return {...state, items: payload}
-  default:
-    return state
+    case "DELETE_ITEM":
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== payload.id),
+      };
+    default:
+      return state;
   }
 }
