11e1650596d55e96c2ee54148ad46818c5e696dc
RTEYL <tyler-w-jones@hotmail.com>
2020-11-25 18:10:50 -0800

    add admin routes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,72 @@
-import React, {Component} from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {connect} from 'react-redux'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Container from 'react-bootstrap/Container'
-import ItemsContainer from './containers/ItemsContainer';
-import NavBar from './components/NavBar'
-import {fetchItems, fetchLoggedInUser} from './actions/Fetch'
-import {CartContainer} from './containers/CartContainer'
-import {Login} from './components/auth/Login';
-import {Logout} from './components/auth/Logout';
-import {SignUp} from './components/auth/SignUp'
-import OrdersContainer from './containers/OrdersContainer'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import ItemsContainer from "./containers/ItemsContainer";
+import { CartContainer } from "./containers/CartContainer";
+import OrdersContainer from "./containers/OrdersContainer";
+import AdminContainer from "./containers/AdminContainer";
+import NavBar from "./components/NavBar";
+import ItemForm from "./components/admin/ItemForm";
+import { Login } from "./components/auth/Login";
+import { Logout } from "./components/auth/Logout";
+import { SignUp } from "./components/auth/SignUp";
+import { fetchItems, fetchLoggedInUser } from "./actions/Fetch";
 
-class App extends Component{
-
-  componentDidMount(){
-    this.props.fetchLoggedInUser()
-    this.props.fetchItems()
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchLoggedInUser();
+    this.props.fetchItems();
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <>
         <Router>
           <NavBar />
           <Container>
             <Switch>
-              <Route exact path='/' component={ItemsContainer}/>
-              <Route exact path='/cart' component={CartContainer}/>
-              <Route exact path='/login' component={Login}/>
-              <Route exact path='/logout' component={Logout}/>
-              <Route exact path='/signup' component={SignUp}/>
-              <Route path='/users/:id/orders' component={OrdersContainer} />
+              {this.props.user.admin && (
+                <>
+                  <Route
+                    exact
+                    path="/admin"
+                    render={(props) => <AdminContainer {...props} />}
+                  />
+                  <Route
+                    path="/admin/items/:itemId"
+                    render={(props) => <ItemForm {...props} />}
+                  />
+                </>
+              )}
+              <Route exact path="/" component={ItemsContainer} />
+              <Route exact path="/cart" component={CartContainer} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/logout" component={Logout} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route path="/users/:id/orders" component={OrdersContainer} />
+              <Route component={ItemsContainer} />
             </Switch>
           </Container>
-      </Router>
+        </Router>
       </>
-    )
+    );
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-     fetchItems: () => dispatch(fetchItems()),
-     fetchLoggedInUser: () => dispatch(fetchLoggedInUser())
-    }
-}
+    fetchItems: () => dispatch(fetchItems()),
+    fetchLoggedInUser: () => dispatch(fetchLoggedInUser()),
+  };
+};
 
 const mapStateToProps = (state) => {
   return {
-    loggedIn: state.users.logged_in
-  }
-}
+    loggedIn: state.users.logged_in,
+    user: state.users.user,
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
