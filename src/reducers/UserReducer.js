7677a265aa9b05b63026a751985fc188d0b6a5df
RTEYL <tyler-w-jones@hotmail.com>
2020-11-25 18:35:54 -0800

    add admin to reduce

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,23 +1,41 @@
 const initialState = {
   user: {},
-  errors: '',
-  logged_in: false
-}
+  errors: "",
+  logged_in: false,
+  isAdmin: false,
+};
 
-export default function UserReducer(state = initialState, { type, payload }){
+export default function UserReducer(state = initialState, { type, payload }) {
   switch (type) {
-  case 'IS_LOGGED_IN':
-    return { ...state, user: payload.user,  logged_in: payload.logged_in }
-  case 'LOG_IN':
-    return { ...state, user: payload.user, logged_in: payload.logged_in, errors: '' }
-  case 'LOG_OUT':
-    return { ...state, user: {}, logged_in: false, errors: '' }
-  case 'ADD_ERROR':
-    return { ...state, errors: payload}
-  case 'REMOVE_ERRORS':
-    return {...state, errors: '' }
+    case "IS_LOGGED_IN":
+      return {
+        ...state,
+        user: payload.user,
+        logged_in: payload.logged_in,
+        isAdmin: payload.user.admin,
+      };
+    case "LOG_IN":
+      return {
+        ...state,
+        user: payload.user,
+        logged_in: payload.logged_in,
+        errors: "",
+        isAdmin: payload.user.admin,
+      };
+    case "LOG_OUT":
+      return {
+        ...state,
+        user: {},
+        logged_in: false,
+        errors: "",
+        isAdmin: false,
+      };
+    case "ADD_ERROR":
+      return { ...state, errors: payload };
+    case "REMOVE_ERRORS":
+      return { ...state, errors: "" };
 
-  default:
-    return state
+    default:
+      return state;
   }
 }
