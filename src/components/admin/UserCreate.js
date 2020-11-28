b816a4b736c28fedd700e1525d1a03d4d0406b61
RTEYL <tyler-w-jones@hotmail.com>
2020-11-27 21:00:19 -0800

    lazy create for admins

---
+++ src/components/admin/UserCreate.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { SignUp } from "../auth/SignUp";
+
+class UserCreate extends Component {
+  render() {
+    return <SignUp />;
+  }
+}
+
+export default UserCreate;
