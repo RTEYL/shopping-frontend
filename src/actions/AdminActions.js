15d08502b3f5d9df79c7479001df9af7675334d5
RTEYL <tyler-w-jones@hotmail.com>
2020-11-27 17:25:26 -0800

    add create action

--- src/actions/AdminActions.js
+++ src/actions/AdminActions.js
@@ -34,3 +34,22 @@ export const updateItem = ({ item, history }) => {
       });
   };
 };
+export const createItem = ({ item, history }) => {
+  let configObj = {
+    credentials: "include",
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(item),
+  };
+  return (dispatch) => {
+    fetch("http://localhost:3000/api/v1/items", configObj)
+      .then((resp) => resp.json())
+      .then((json) => {
+        dispatch({ type: "ADD_ITEM", payload: json.item });
+        history.push("/admin");
+      });
+  };
+};
