9fbe8dcff444cf33bb461590fc794fa507967fa0
RTEYL <tyler-w-jones@hotmail.com>
2020-11-28 17:51:39 -0800

    use tab indexes

--- src/containers/AdminContainer.js
+++ src/containers/AdminContainer.js
@@ -1,30 +1,34 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Tab, Tabs } from "react-bootstrap";
 import ItemsList from "../components/admin/ItemsList";
 import UsersList from "../components/admin/UsersList";
 
 const AdminContainer = (props) => {
   return (
-    <>
-      <Button
-        onClick={() => {
-          props.history.push(`/admin/items/create`);
-        }}
-        size="sm"
-        variant="info">
-        Create New Item
-      </Button>
-      <ItemsList {...props} />
-      <Button
-        onClick={() => {
-          props.history.push(`/admin/users/create`);
-        }}
-        size="sm"
-        variant="info">
-        Create New User
-      </Button>
-      <UsersList {...props} />
-    </>
+    <Tabs defaultActiveKey="Items" id="admin-tab-pannel">
+      <Tab eventKey="Items" title="Items">
+        <Button
+          onClick={() => {
+            props.history.push(`/admin/items/create`);
+          }}
+          size="sm"
+          variant="info">
+          Create New Item
+        </Button>
+        <ItemsList {...props} />
+      </Tab>
+      <Tab eventKey="Users" title="Users">
+        <Button
+          onClick={() => {
+            props.history.push(`/admin/users/create`);
+          }}
+          size="sm"
+          variant="info">
+          Create New User
+        </Button>
+        <UsersList {...props} />
+      </Tab>
+    </Tabs>
   );
 };
 
