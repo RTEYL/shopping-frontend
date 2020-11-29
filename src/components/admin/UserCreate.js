288f530ea53f301595e769d4d647b7d39b46bc70
RTEYL <tyler-w-jones@hotmail.com>
2020-11-28 17:51:29 -0800

    complete form

--- src/components/admin/UserCreate.js
+++ src/components/admin/UserCreate.js
@@ -1,9 +1,87 @@
 import React, { Component } from "react";
-import { SignUp } from "../auth/SignUp";
+import { Alert, Form } from "react-bootstrap";
 
 class UserCreate extends Component {
+  state = {
+    email: "",
+    password: "",
+    password_confirmation: "",
+    admin: false,
+  };
+
+  handleChange = (event) => {
+    let { name, value } = event.target;
+    if (name === "admin") {
+      value = value === "true" ? true : false;
+    }
+    this.setState({
+      [name]: value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let configObj = {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ user: this.state }),
+    };
+    fetch("http://localhost:3000/api/v1/users", configObj)
+      .then((resp) => resp.json())
+      .then((json) => {
+        if (!!json.errors) {
+          throw (new Error().message = json.errors);
+        } else {
+          this.props.history.push("/admin");
+        }
+      })
+      .catch((err) => {
+        <Alert variant="danger">{err}</Alert>;
+      });
+  };
   render() {
-    return <SignUp />;
+    const { email, password, password_confirmation } = this.state;
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          <input
+            placeholder="Confirm Password"
+            type="password"
+            name="password_confirmation"
+            value={password_confirmation}
+            onChange={this.handleChange}
+          />
+          <br />
+          <br />
+          Admin?
+          <select name="admin" onChange={this.handleChange}>
+            <option value={false}>False</option>
+            <option value={true}>True</option>
+          </select>
+          <br />
+          <br />
+          <button type="submit">Create User</button>
+        </Form>
+      </div>
+    );
   }
 }
 
