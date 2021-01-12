812a6ff10b44c5a74f8256c1704460a74c34085e
RTEYL <tyler-w-jones@hotmail.com>
2021-01-12 15:41:56 -0800

    fix nested tag issue

--- src/components/orders/Order.js
+++ src/components/orders/Order.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Card, Col } from 'react-bootstrap'
+import React from "react";
+import { Card, Col } from "react-bootstrap";
 
 const Order = (props) => {
   return (
-    <Col sm={6.5} md={6} lg={4} xl={3} >
-      <Card bg='light'>
+    <Col sm={6.5} md={6} lg={4} xl={3}>
+      <Card bg="light">
         <Card.Body>
           <Card.Title>Order number: {props.order.id}</Card.Title>
           <Card.Text>
-            <p>Purchase date: {props.order.purchase_date}</p>
-            <p>Shipped to: </p>
-            <p>Address: {props.order.line1}, {props.order.city}, {props.order.state}, {props.order.postal_code} </p>
-            <p>Email: <em>{props.order.email}</em></p>
+            <p>
+              Purchase date: {props.order.purchase_date}
+              <br />
+              Shipped to: {props.order.recipient_name}
+              <br />
+              Address: {props.order.line1}, {props.order.city}{" "}
+              {props.order.state}, {props.order.postal_code}
+              <br />
+              Email: <em>{props.order.email}</em>
+            </p>
           </Card.Text>
         </Card.Body>
       </Card>
     </Col>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;
